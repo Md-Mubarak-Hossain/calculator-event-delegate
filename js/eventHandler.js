e6d1597b8a7e987d782d50c7cf4cbf3e721f45f4
Md-Mubarak-Hossain <hossainmubarak477@gmail.com>
2022-08-24 15:34:30 +0600

    added

--- js/eventHandler.js
+++ js/eventHandler.js
@@ -3,20 +3,27 @@ function calcNumber(event) {
     const btn = event.target.value;
     const textField = document.getElementById('input-text');
     const textFieldValue = textField.value;
+
     if (btn === '<') {
         const textSplit = textFieldValue.split('');
         textSplit.pop();
         const result = textSplit.join('');
         textField.value = result;
     }
+    else if (btn === '>') {
+        const textSplit = textFieldValue.split('');
+        textSplit.shift();
+        const result = textSplit.join('');
+        textField.value = result;
+    }
 
+    else if (btn === 'ac') {
+        textField.value = '';
+    }
     else if (btn === 'C') {
         textField.value = '';
     }
-    else if (btn === 'div') {
-        textField.value += '';
-        // return '';
-    }
+
     else if (btn === '=') {
         const result = eval(textField.value);
         textField.value = result;
